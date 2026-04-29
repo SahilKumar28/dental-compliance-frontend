f2ccc885acd6f4ead766a15d7b64012ade18a848
SahilKumar28 <sahilmukeshkumar01@gmail.com>
2026-04-29 13:53:15 +0500

    changes

--- lib/api/auth.service.ts
+++ lib/api/auth.service.ts
@@ -5,19 +5,35 @@ import { setSession } from "../auth/session";
 import { resolvePortal } from "../auth/resolvePortal";
 
 export async function login(email: string, password: string) {
+
+  const store = useAuthStore.getState();
+
   const res = await apiClient.post("/auth/login", {
     email,
     password,
   });
-  const { access_token, user } = res.data.data;
+  const { access_token, user } = res.data;
 
   setAccessToken(access_token);
 
   setSession(user)
 
 
-  const activePortal = resolvePortal(user.roles)
-  useAuthStore.getState().setActivePortal(activePortal);
+  const { portal, active_role_name, active_practice_id } = resolvePortal(user.roles);
+  console.log(portal, active_practice_id, active_role_name)
+  store.setActivePortal(portal);
+
+  const matchedRole = user.roles.find((role: any) => {
+    return (
+      role.role_name === active_role_name &&
+      role.practice_id === active_practice_id
+    );
+  });
+
+  console.log(matchedRole?.theme)
+  if (matchedRole?.theme) {
+    store.setTheme(matchedRole.theme);
+  }
 
   // useAuthStore.getState().setUser(user)
 
